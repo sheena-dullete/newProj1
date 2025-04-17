e57b47cbe5658ec18b7d73298f121c9ada7a084b
sheena-dullete <sheenadullete1226@gmail.com>
2025-04-17 19:58:13 +0800

    new console.writeline

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Philippines, Hello, World!");
 Console.WriteLine("New line of Code");
+Console.WriteLine("Another Modification");
