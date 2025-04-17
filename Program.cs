fdeddddd6f4e3bfdc15d676032dc21e925a839ed
sheena-dullete <sheenadullete1226@gmail.com>
2025-04-17 20:34:06 +0800

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Philippines, Hello, World!");
-Console.WriteLine("New line of Code");
-Console.WriteLine("Another Modification");
-Console.WriteLine("Another Modification");
-Console.WriteLine("Another Modification");
-Console.WriteLine("Another Modification");
-Console.WriteLine("Another Modification");
-Console.WriteLine("Another Modification");
+﻿
+void firstMethod()
+{
+    // code 
+}
+
+void secondMethod()
+{
+    // code 
+}
+
+void thirdMethod()
+{
+    // code 
+}
